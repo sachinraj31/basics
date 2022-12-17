a0a0fef408a93a7135425e77298bd7378239be21
sachinraj31 <sachinraj31011998@gmail.com>
2022-12-17 11:45:08 +0530

    second commit

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ titles.textContent = "basisc html";
 let add = document.getElementById("sa");
 add.style.color = "green";
 add.style.fontWeight = "bold";
+
+let item = document.getElementsByClassName("list-group-item");
+item[3].style.backgroundColor = "green";
+
+for (let i = 0; i < item.length; i++) {
+  item[i].style.fontWeight = "bold";
+}
