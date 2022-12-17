4bd5ccc3d15c881e7fb255a00232fa8acf63be42
sachinraj31 <sachinraj31011998@gmail.com>
2022-12-17 13:54:16 +0530

    fourth commit

--- main.js
+++ main.js
@@ -15,3 +15,16 @@ let li = document.getElementsByTagName("li");
 for (let i = 0; i < li.length; i++) {
   li[i].style.fontWeight = "bold";
 }
+let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+secondItem.style.backgroundColor = "green";
+
+let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.visibility = "hidden";
+
+let second = document.querySelectorAll(".list-group-item");
+second[1].style.color = "green";
+
+let odd = document.querySelectorAll(".list-group-item:nth-child(odd)");
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "green";
+}
