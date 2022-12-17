ba826c19bde832a0a1116fe172c08ff843c72a6b
sachinraj31 <sachinraj31011998@gmail.com>
2022-12-17 11:23:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let titles = document.querySelector("title");
+titles.textContent = "basisc html";
+
+let add = document.getElementById("sa");
+add.style.color = "green";
+add.style.fontWeight = "bold";
